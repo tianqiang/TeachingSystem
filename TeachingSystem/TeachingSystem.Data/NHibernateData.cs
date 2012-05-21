d5f41675679c87c4bfdea8b84da533d33e706408
shaotianqiang <151695276@qq.com>
2012-05-21 22:04:44 +0800

    版本1
    Signed-off-by: shaotianqiang <151695276@qq.com>

---
+++ TeachingSystem/TeachingSystem.Data/NHibernateData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NHibernate;
+using TeachingSystem.Domain.Entities;
+
+namespace TeachingSystem.Data
+{
+    public class NHibernateData
+    {
+        protected ISession Session { get; set; }
+        public NHibernateData()
+        {
+            Session = new NHibernateHelper().GetSession();
+        }
+        public void CreateStudentData(UserData studentInfo)
+        {
+            Session.Save(studentInfo);
+        }
+        public UserData GetUserDataByNum(string StudentNum)
+        {
+            return Session.Get<UserData>(StudentNum);
+        }
+        public void Close()
+        {
+            Session.Close();
+        }
+    }
+}
